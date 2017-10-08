c20e1c8489a953fb1e84f1f9ffe14e23dc2dbab7
Rusty Mellinger <rusty.mellinger@gmail.com>
2017-10-08 12:48:24 -0600

    Simplify execution by running stimulus on main thread.

--- src/io/rmel/mock/Expectation.java
+++ src/io/rmel/mock/Expectation.java
@@ -3,27 +3,10 @@ package io.rmel.mock;
 final class Expectation {
 
   static void run(Runnable expectations, Runnable test) {
-    final ThrowableHolder h = new ThrowableHolder();
     Thread expectationsThread = new Thread(expectations, "expectations");
     expectationsThread.setUncaughtExceptionHandler((th, ex) -> {});
     expectationsThread.start();
 
-    Thread testThread = new Thread(test, "test");
-    testThread.setUncaughtExceptionHandler(
-        (th, ex) -> {h.value = ex;});
-    testThread.start();
-
-    try { 
-      testThread.join();
-      if (h.value != null) {
-        throw new UnexpectedException(h.value);
-      }
-    } catch (InterruptedException e) {
-      throw new RuntimeException("Interrupted while running test.", e);
-    }
-  }
-
-  static class ThrowableHolder {
-    Throwable value = null;
+    test.run();
   }
 }
